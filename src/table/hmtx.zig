6aa1554d67a4bbaa54c7cd9c6f936a88d87d0c72
kanno <812137533@qq.com>
2025-06-30 13:58:23 +0800

    chore: clean code

--- src/table/hmtx.zig
+++ src/table/hmtx.zig
@@ -11,14 +11,48 @@ pub const HtmxTable = struct {
     allocator: Allocator,
     byte_reader: *reader.ByteReader,
     parsed_tables: *ParsedTables,
+    h_metrics: []LongHorMetric,
+
+    left_side_bearings: []i16,
+
+    pub const LongHorMetric = packed struct {
+        advance_width: u16,
+        lsb: i16,
+    };
 
     fn parse(ptr: *anyopaque) !void {
         const self: *Self = @ptrCast(@alignCast(ptr));
-        _ = self;
+        const number_of_hmetrics = self.parsed_tables.get_hhea().?.number_of_hmetrics;
+        const num_glyphs = self.parsed_tables.get_maxp().?.num_glyphs;
+
+        var h_metrics = try self.allocator.alloc(LongHorMetric, number_of_hmetrics);
+
+        errdefer self.allocator.free(h_metrics);
+
+        for (0..number_of_hmetrics) |i| {
+            h_metrics[i] = LongHorMetric{
+                .advance_width = try self.byte_reader.read_u16_be(),
+                .lsb = try self.byte_reader.read_i16_be(),
+            };
+        }
+
+        const left_side_bearings_len = if (num_glyphs > number_of_hmetrics) num_glyphs - number_of_hmetrics else 0;
+
+        var left_side_bearings = try self.allocator.alloc(i16, left_side_bearings_len);
+
+        errdefer self.allocator.free(left_side_bearings);
+
+        for (0..left_side_bearings_len) |i| {
+            left_side_bearings[i] = try self.byte_reader.read_i16_be();
+        }
+        self.h_metrics = h_metrics;
+        self.left_side_bearings = left_side_bearings;
     }
 
     fn deinit(ptr: *anyopaque) void {
         const self: *Self = @ptrCast(@alignCast(ptr));
+        self.allocator.free(self.h_metrics);
+        self.allocator.free(self.left_side_bearings);
         self.allocator.destroy(self);
     }
 
@@ -35,6 +69,16 @@ pub const HtmxTable = struct {
             .ptr = self,
             .vtable = &.{ .parse = parse, .deinit = deinit },
         };
+    }
+
+    pub fn get_advance_width(self: *Self, glyph_index: u16) void {
+        _ = self;
+        _ = glyph_index;
+    }
+
+    pub fn get_left_side_bearing(self: *Self, glyph_index: u16) void {
+        _ = self;
+        _ = glyph_index;
     }
 };
 
