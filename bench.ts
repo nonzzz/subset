1319b5b33543b107caffb4c9b3540189f4a93578
kanno <812137533@qq.com>
2025-07-03 16:59:46 +0800

    chore: add bench test

---
+++ bench.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import fs from 'fs'
+import { bench, group, run } from 'mitata'
+import opentype from 'opentype.js'
+import path from 'path'
+import { createSubsetEngine } from './bindings/wasm/javascript'
+
+const wasmPath = path.join(__dirname, 'zig-out', 'ttf.wasm')
+const ttfPath = path.join(__dirname, 'fonts', 'LXGWBright-Light.ttf')
+const fontData = new Uint8Array(fs.readFileSync(ttfPath))
+const wasmBinary = fs.readFileSync(wasmPath)
+
+const testText = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789汉字测试段落The quick brown fox jumps over the lazy dog.'
+
+const wasmEngine = createSubsetEngine(wasmBinary)
+wasmEngine.loadFont(fontData)
+const otFont = opentype.parse(fs.readFileSync(ttfPath).buffer)
+
+group('get glyph id for codepoint', () => {
+  bench('WASM', () => {
+    for (const char of testText) {
+      const codePoint = char.codePointAt(0)!
+      wasmEngine.getGlyphIdForCodepoint(codePoint)
+    }
+  })
+  bench('opentype.js', () => {
+    for (const char of testText) {
+      const codePoint = char.codePointAt(0)!
+      otFont.charToGlyphIndex(String.fromCodePoint(codePoint))
+    }
+  })
+})
+
+group('get glyph name', () => {
+  const glyphIds = Array.from({ length: 10000 }, (_, i) => i + 1)
+  bench('WASM', () => {
+    for (const id of glyphIds) {
+      wasmEngine.getGlyphName(id)
+    }
+  })
+  bench('opentype.js', () => {
+    for (const id of glyphIds) {
+      otFont.glyphs.get(id)?.name
+    }
+  })
+})
+
+run().catch(console.error)
