4b2dbfdd813b09519af2dc2417ae20115b29b76d
kanno <812137533@qq.com>
2025-07-03 18:06:19 +0800

    chore: clean code

--- build.zig
+++ build.zig
@@ -5,11 +5,14 @@ pub fn build(b: *std.Build) !void {
     const build_steps = .{
         .unit_test = b.step("test", "Run unit tests"),
         .wasm = b.step("wasm", "Build WebAssembly bindings"),
+        .node = b.step("node", "Build Node.js bindings"),
     };
 
     build_unit_test(b, build_steps.unit_test);
 
     build_wasm_bindings(b, build_steps.wasm);
+
+    build_node_bindings(b, build_steps.node);
 }
 
 fn build_unit_test(b: *std.Build, step_unit_test: *std.Build.Step) void {
@@ -50,4 +53,29 @@ fn build_wasm_bindings(b: *std.Build, step_wasm: *std.Build.Step) void {
             "ttf.wasm",
         },
     )).step);
+}
+
+fn build_node_bindings(b: *std.Build, step_node: *std.Build.Step) void {
+    const node_target = b.resolveTargetQuery(.{});
+
+    const node_generate = b.addSharedLibrary(.{
+        .name = "ttf",
+        .root_source_file = b.path("bindings/javascript/mod.zig"),
+        .target = node_target,
+        .optimize = .ReleaseSmall,
+    });
+
+    const ttf_mod = b.addModule("ttf", .{
+        .root_source_file = b.path("src/lib.zig"),
+    });
+    node_generate.root_module.addImport("ttf", ttf_mod);
+
+    node_generate.addSystemIncludePath(b.path("node_modules/node-api-headers/include"));
+
+    node_generate.linker_allow_shlib_undefined = true;
+    node_generate.linkLibC();
+
+    step_node.dependOn(&b.addInstallFile(node_generate.getEmittedBin(), b.pathJoin(&.{
+        "ttf.node",
+    })).step);
 }
